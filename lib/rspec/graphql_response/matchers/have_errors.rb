63d7975d33d61db7bf6ae9ee5a7413ed32f31747
River Lynn Bailey <riverlynnbailey@gmail.com>
2021-02-20 10:10:53 -0600

    updated have_errors matcher to include failure message and failure message when negated

--- lib/rspec/graphql_response/matchers/have_errors.rb
+++ lib/rspec/graphql_response/matchers/have_errors.rb
@@ -6,8 +6,17 @@ module RSpec
 
         matcher :have_errors do
           match do |response|
-            have_errors = Validators::HaveErrors.new(response, expected_messages: @messages)
-            return have_errors.validate
+            @have_errors = Validators::HaveErrors.new(response, expected_messages: @messages)
+
+            return @have_errors.validate
+          end
+
+          failure_message do |response|
+            @have_errors.reason
+          end
+
+          failure_message_when_negated do |response|
+            @have_errors.negated_reason
           end
 
           chain :with_messages do |*messages|
