0f6aafd5d9a32809e1e7934fcb0f441686306c8a
River Lynn Bailey <riverlynnbailey@gmail.com>
2021-02-21 15:07:25 -0600

    cache the result of helper methods, and reset the cache to nil after(:each)

--- lib/rspec/graphql_response/helpers.rb
+++ lib/rspec/graphql_response/helpers.rb
@@ -3,7 +3,13 @@ module RSpec
     def self.add_helper(name, &helper)
       helper_module = Module.new do |mod|
         mod.define_method(name) do |*args|
-          self.instance_exec(*args, &helper)
+          @result ||= self.instance_exec(*args, &helper)
+        end
+      end
+
+      RSpec.configure do |config|
+        config.after(:each) do
+          helper_module.instance_variable_set(:@result, nil)
         end
       end
 
