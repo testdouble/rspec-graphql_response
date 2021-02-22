4d9a45a64e20f070f5d551d3f1fdde4ebe6272d1
River Lynn Bailey <river.bailey@testdouble.com>
2021-02-22 09:29:56 -0600

    re-built the .to_not have_errors validation and error messages

--- spec/graphql_response/validators/have_errors_negated_spec.rb
+++ spec/graphql_response/validators/have_errors_negated_spec.rb
@@ -43,7 +43,7 @@ RSpec.describe RSpec::GraphQLResponse, "validator#not_have_errors" do
     end
 
     it "provides a reason" do
-      expect(not_have_errors.negated_reason).to eq("Expected response not to have errors, but found\n\t#{response["errors"].inspect}")
+      expect(not_have_errors.negated_reason).to eq("Expected response not to have errors, but found\n\t#{actual_messages.inspect}")
     end
   end
 
@@ -53,13 +53,10 @@ RSpec.describe RSpec::GraphQLResponse, "validator#not_have_errors" do
     it "is valid" do
       expect(not_have_errors.valid?).to be_truthy
     end
-
-    it "provides a reason" do
-      expect(not_have_errors.negated_reason).to eq("Expected response not to have errors, but found\n\t#{actual_messages.inspect}")
-    end
   end
 
-  context "correct message expected" do
+  context "message expected not to be there" do
+    let(:response) { {} }
     let(:expected_messages) { ["No query string was present"] }
 
     it "is valid" do
@@ -75,68 +72,37 @@ RSpec.describe RSpec::GraphQLResponse, "validator#not_have_errors" do
     end
 
     it "provides a reason" do
-      expect(not_have_errors.reason).to eq("Expected\n\t#{expected_messages.inspect}\nbut found\n\t#{actual_messages.inspect}")
-    end
-
-    it "provides a negated reason" do
-      expect(not_have_errors.negated_reason).to eq("Expected not to find\n\t#{expected_messages.inspect}\nbut found\n\t#{actual_messages.inspect}")
-    end
-  end
-
-  context "incorrect message expected" do
-    let(:expected_messages) { ["wrong error message"] }
-
-    it "is not valid" do
-      expect(not_have_errors.valid?).to be_falsey
-    end
-
-    it "provides a reason" do
-      expect(not_have_errors.reason).to eq("Expected\n\t#{expected_messages.inspect}\nbut found\n\t#{actual_messages.inspect}")
-    end
-
-    it "provides a negated reason" do
-      expect(not_have_errors.negated_reason).to eq("Expected not to find\n\t#{expected_messages.inspect}\nbut found\n\t#{actual_messages.inspect}")
+      expect(not_have_errors.negated_reason).to eq("Expected not to find any of\n\t#{expected_messages.inspect}\nbut found\n\t#{actual_messages.inspect}")
     end
   end
 
   context "with expected error count matching actual error count" do
     let(:expected_count) { 1 }
 
-    it "is valid" do
-      expect(not_have_errors.valid?).to be_truthy
+    it "is not valid" do
+      expect(not_have_errors.valid?).to be_falsey
+    end
+
+    it "provides a reason" do
+      expect(not_have_errors.negated_reason).to eq("Expected response not to have #{expected_count} errors, but found #{actual_count}\n\t#{actual_messages}")
     end
   end
 
   context "with expected error count not matching actual error count" do
     let(:expected_count) { 2 }
 
-    it "is not valid" do
-      expect(not_have_errors.valid?).to be_falsey
-    end
-
-    it "provides a reason" do
-      expect(not_have_errors.reason).to eq("Expected response to have #{expected_count} errors, but found #{actual_count}")
-    end
-
-    it "provides a negated reason" do
-      expect(not_have_errors.negated_reason).to eq("Expected response not to have #{expected_count} errors, but found #{actual_count}")
+    it "is valid" do
+      expect(not_have_errors.valid?).to be_truthy
     end
   end
 
   context "with unmatched error count and unmatched messages" do
     let(:expected_count) { 3 }
-    let(:expected_messages) { ["No query string was present", "Error 2"] }
+    let(:expected_messages) { ["Error 1", "Error 2"] }
 
-    it "is not valid" do
-      expect(not_have_errors.valid?).to be_falsey
-    end
-
-    it "provides the unmatched error count reason" do
-      expect(not_have_errors.reason).to eq("Expected response to have #{expected_count} errors, but found #{actual_count}")
-    end
-
-    it "provides the negated unmatched error count reason" do
-      expect(not_have_errors.negated_reason).to eq("Expected response not to have #{expected_count} errors, but found #{actual_count}")
+    it "is valid" do
+      expect(not_have_errors.negated_reason).to be_nil
+      expect(not_have_errors.valid?).to be_truthy
     end
   end
 
@@ -149,11 +115,20 @@ RSpec.describe RSpec::GraphQLResponse, "validator#not_have_errors" do
     end
 
     it "provides the unmatched error count reason" do
-      expect(not_have_errors.reason).to eq("Expected\n\t#{expected_messages.inspect}\nbut found\n\t#{actual_messages.inspect}")
+      expect(not_have_errors.negated_reason).to eq("Expected response not to have #{expected_count} errors, but found #{actual_count}\n\t#{actual_messages.inspect}")
+    end
+  end
+
+  context "with unmatched error count and matched messages" do
+    let(:expected_count) { 2 }
+    let(:expected_messages) { ["No query string was present"] }
+
+    it "is not valid" do
+      expect(not_have_errors.valid?).to be_falsey
     end
 
-    it "provides the negated unmatched error count reason" do
-      expect(not_have_errors.negated_reason).to eq("Expected not to find\n\t#{expected_messages.inspect}\nbut found\n\t#{actual_messages.inspect}")
+    it "provides the unmatched error message reason" do
+      expect(not_have_errors.negated_reason).to eq("Expected not to find any of\n\t#{expected_messages.inspect}\nbut found\n\t#{actual_messages.inspect}")
     end
   end
 end
