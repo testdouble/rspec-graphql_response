525540c5402d9f64523867880647e8d44e776858
River Lynn Bailey <riverlynnbailey@gmail.com>
2021-02-28 13:44:18 -0600

    better organizing validate vs validate_negated. finishing core specs for add validators and running them

--- spec/graphql_response/validators/add_validator_spec.rb
+++ spec/graphql_response/validators/add_validator_spec.rb
@@ -7,46 +7,111 @@ RSpec.describe RSpec::GraphQLResponse, "#add_validator" do
       failure_message :negated_example, "example negated failure message"
       failure_message :negated_does_not_pass, "negated does not pass"
 
-      validate do |actual, fail_example: false, fail_negated_example: false|
+      validate do |actual, fail_example: false|
         next fail_validation(:example) if fail_example
-        next fail_validation(:negated_example) if fail_negated_example
 
         actual = actual || {}
         it_passes = actual.fetch("pass", false)
+        next fail_validation(:does_not_pass) unless it_passes
 
         pass_validation
       end
 
-      validate_negated do |actual, fail_example: false, fail_negated_example: false|
-        next fail_validation(:example) unless fail_example
-        next fail_validation(:negated_example) unless fail_negated_example
+      validate_negated do |actual, fail_negated_example: false|
+        next fail_validation(:negated_example) if fail_negated_example
 
         actual = actual || {}
         it_passes = actual.fetch("pass", false)
-
-        fail_validation(:negated_does_not_pass) if it_passes
+        next fail_validation(:negated_does_not_pass) if it_passes
 
         pass_validation
       end
     end
   end
 
-  let(:fail_example) { false }
-  let(:fail_negated_example) { false }
-  let(:actual) { { pass: true } }
+  let(:validator) do
+    RSpec::GraphQLResponse.validator(:test_validator)
+  end
 
   describe "#validate" do
-    subject(:result) do
-      validator = RSpec::GraphQLResponse.validator(:test_validator)
-      validator.validate(
-        actual,
-        fail_example: fail_example,
-        fail_negated_example: fail_negated_example
-      )
+    let(:fail_example) { false }
+
+    let(:actual) do
+      { "pass" => true }
     end
 
-    it "passes validation" do
-      expect(result.valid?).to be_truthy
+    subject(:result) do
+      validator.validate(actual, fail_example: fail_example)
+    end
+
+    context "passes" do
+      it "passes validation" do
+        expect(result.valid?).to be_truthy
+      end
+
+      it "does not provide a reason" do
+        expect(result.reason).to be_nil
+      end
+    end
+
+    context "does not pass" do
+      let(:actual) do
+        { pass: false }
+      end
+
+      it "fails validation" do
+        expect(result.valid?).to be_falsey
+      end
+
+      it "provides a reason" do
+        expect(result.reason).to eq("does not pass")
+      end
+    end
+
+    context "fails example" do
+      let(:fail_example) { true }
+
+      it "does not pass validation" do
+        expect(result.valid?).to be_falsey
+      end
+
+      it "provides a reason" do
+        expect(result.reason).to eq("example failure message")
+      end
+    end
+  end
+
+  describe "#validate_negated" do
+    let(:fail_negated_example) { false }
+
+    let(:actual) do
+      { pass: false }
+    end
+
+    subject(:result) do
+      validator.validate_negated(actual, fail_negated_example: fail_negated_example)
+    end
+
+    context "negated pass" do
+      it "passes negated validation" do
+        expect(result.valid?).to be_truthy
+      end
+
+      it "does not provide a reason" do
+        expect(result.reason).to be_nil
+      end
+    end
+
+    context "fail negated example" do
+      let(:fail_negated_example) { true }
+
+      it "does not validate" do
+        expect(result.valid?).to be_falsey
+      end
+
+      it "provides a reason" do
+        expect(result.reason).to eq("example negated failure message")
+      end
     end
   end
 
