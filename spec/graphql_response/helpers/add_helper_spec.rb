4d8754af1fa6c25f685f53615d3d63690f742186
River Lynn Bailey <riverlynnbailey@gmail.com>
2021-03-08 19:27:50 -0600

    basic specs for add_helper

---
+++ spec/graphql_response/helpers/add_helper_spec.rb
@@ -0,0 +1,24 @@
+RSpec.describe RSpec::GraphQLResponse, "#add_helper", type: :graphql do
+  before(:all) do
+    RSpec::GraphQLResponse.add_helper :example_helper do |p1|
+      @p1 = p1
+    end
+  end
+
+  it "makes the helper method callable" do
+    expect { example_helper }.to_not raise_error
+  end
+
+  it "adds the helper to the current scope" do
+    example_helper("foo")
+
+    expect(@p1).to eq("foo")
+  end
+
+  context "nested scope" do
+    it "makes the helper available" do
+      expect { example_helper("bar") }.to_not raise_error
+      expect(@p1).to eq("bar")
+    end
+  end
+end
