ceb1c6900852f966977fe7504bb1817ed9bdfa18
River Lynn Bailey <riverlynnbailey@gmail.com>
2021-03-08 19:40:11 -0600

    failing spec to show proc as value for graphql_context

--- spec/graphql_response/helpers/graphql_context_spec.rb
+++ spec/graphql_response/helpers/graphql_context_spec.rb
@@ -24,4 +24,27 @@ RSpec.describe RSpec::GraphQLResponse, "graphql_variables helper", type: :graphq
       ]
     )
   end
+
+  context "proc as value" do
+    let(:sample_value) { "sample value" }
+
+    graphql_context do
+      {
+        current_user: sample_value
+      }
+    end
+
+    it "provides access to let vars through a proc as value" do
+      expect(response).to_not have_errors
+
+      expect(response["data"]).to include(
+        "characters" => [
+          { "id" => "1", "name" => "Jam" },
+          { "id" => "2", "name" => "Redemption" },
+          { "id" => "3", "name" => "Pet" },
+          { "id" => "4", "name" => "sample value" }
+        ]
+      )
+    end
+  end
 end
