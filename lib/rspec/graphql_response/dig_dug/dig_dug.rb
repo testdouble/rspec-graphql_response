5b412d15378fba0d5a87a090cd2f06d7a703bedd
River Lynn Bailey <riverlynnbailey@gmail.com>
2021-03-13 15:50:55 -0600

    first passing spec of first level dig with a hash

--- lib/rspec/graphql_response/dig_dug/dig_dug.rb
+++ lib/rspec/graphql_response/dig_dug/dig_dug.rb
@@ -1,7 +1,21 @@
 module RSpec
   module GraphQLResponse
     class DigDug
+      attr_reader :dig_pattern
 
+      def initialize(dig_pattern)
+        @dig_pattern = parse_dig_pattern(dig_pattern)
+      end
+
+      def dig(data)
+        data[dig_pattern]
+      end
+
+      private
+
+      def parse_dig_pattern(pattern)
+        pattern[0].to_s
+      end
     end
   end
 end
