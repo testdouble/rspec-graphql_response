75f49528a9160345b459901207e0bf3a7d2d91ce
River Lynn Bailey <riverlynnbailey@gmail.com>
2021-03-13 15:57:26 -0600

    core specs to outline the feature set of dig_dug

--- spec/graphql_response/dig_dug/dig_dug_spec.rb
+++ spec/graphql_response/dig_dug/dig_dug_spec.rb
@@ -66,46 +66,69 @@ RSpec.describe RSpec::GraphQLResponse::DigDug do
     end
   end
 
-  it "can dig through an array" do
-    expect(response_data :characters, :friends).to include(
-      { "id" => "2", "name" => "Redemption" },
-      { "id" => "1", "name" => "Jam" },
-      { "id" => "3", "name" => "Pet" },
-      { "id" => "2", "name" => "Redemption" }
-    )
-  end
+  context "dig through an array" do
+    let(:dig_pattern) { [:characters, :friends] }
 
-  it "can dig through an array to nested fields" do
-    expect(response_data :characters, :friends, :name).to include(
-      "Redemption",
-      "Jam",
-      "Pet"
-    )
-  end
-
-  it "can dig into an Array at the specified index" do
-    expect(response_data characters: [1]).to eq(
-      "id" => "2",
-      "name" => "Redemption",
-      "friends" => [
+    it "returns the correct data" do
+      expect(dig).to include(
+        { "id" => "2", "name" => "Redemption" },
         { "id" => "1", "name" => "Jam" },
-        { "id" => "3", "name" => "Pet" }
-      ]
-    )
+        { "id" => "3", "name" => "Pet" },
+        { "id" => "2", "name" => "Redemption" }
+      )
+    end
   end
 
-  it "can dig multiple levels into an Array at the specified index" do
-    expect(response_data characters: [1], friends: [0]).to include(
-      { "id" => "1", "name" => "Jam" },
-    )
+  context "dig through an array to nested fields" do
+    let(:dig_pattern) { [:characters, :friends, :name] }
+
+    it "returns the correct data" do
+      expect(dig).to include(
+        "Redemption",
+        "Jam",
+        "Pet"
+      )
+    end
   end
 
-  it "can dig into a Hash that came through an Array" do
-    expect(response_data characters: [0], friends: [:name]).to eq(["Redemption"])
+  context "dig into an Array at the specified index" do
+    let(:dig_pattern) { [characters: [1]] }
+
+    it "returns the correct data" do
+      expect(dig).to eq(
+        "id" => "2",
+        "name" => "Redemption",
+        "friends" => [
+          { "id" => "1", "name" => "Jam" },
+          { "id" => "3", "name" => "Pet" }
+        ]
+      )
+    end
   end
 
-  it "can dig multiple nested levels of hash and Array" do
-    expect(response_data(:characters, {friends: [0]}, :name)).to eq "Jam"
+  context "dig multiple levels into an Array at the specified index" do
+    let(:dig_pattern) { [characters: [1], friends: [0]] }
+
+    it "returns the correct data" do
+      expect(dig).to include(
+        { "id" => "1", "name" => "Jam" }
+      )
+    end
   end
 
+  context "dig into a Hash that came through an Array" do
+    let(:dig_pattern) { [characters: [0], friends: [:name]] }
+
+    it "returns the correct data" do
+      expect(dig).to eq(["Redemption"])
+    end
+  end
+
+  context "dig multiple nested levels of hash and Array" do
+    let(:dig_pattern) { [:characters, {friends: [0]}, :name] }
+
+    it "returns the correct data" do
+      expect(dig).to eq "Jam"
+    end
+  end
 end
