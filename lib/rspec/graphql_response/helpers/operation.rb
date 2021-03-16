eba2622ff2849175c012edb949ec3d5ed9d644a6
Daniel Flynn <daniel@testdouble.com>
2021-03-16 10:08:03 -0400

    log: add warning of soon to come deprecation

--- lib/rspec/graphql_response/helpers/operation.rb
+++ lib/rspec/graphql_response/helpers/operation.rb
@@ -1,4 +1,5 @@
 RSpec::GraphQLResponse.add_helper :operation do |name|
+  warn 'WARNING: operation has been deprecated in favor of response_data. This helper will be removed in v0.5'
   return nil unless response.is_a? Hash
 
   response.dig("data", name.to_s)
