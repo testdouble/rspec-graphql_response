55a6924b6125b767189a80a5f92f658f6a626262
Daniel Flynn <daniel@testdouble.com>
2021-05-26 15:42:06 -0400

    wip: cleanup pry

--- spec/graphql_response/helpers/with_schema_spec.rb
+++ spec/graphql_response/helpers/with_schema_spec.rb
@@ -13,7 +13,6 @@ RSpec.describe RSpec::GraphQLResponse, "helper#with_schema", type: :graphql do
   GQL
 
   it "queries the correct schema with employees" do
-    binding.pry
       expect(response).to_not be_nil
       expect(response_data).to include(
         "employees" => [
