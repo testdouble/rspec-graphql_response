5b5a72ca55e87a4c7bf03e65f1e8bfaee71c700f
Daniel Flynn <daniel@testdouble.com>
2021-05-26 15:31:36 -0400

    wip: naive schema set

--- spec/graphql_response/helpers/with_schema_spec.rb
+++ spec/graphql_response/helpers/with_schema_spec.rb
@@ -13,6 +13,8 @@ RSpec.describe RSpec::GraphQLResponse, "helper#with_schema", type: :graphql do
   GQL
 
   it "queries the correct schema with employees" do
+    binding.pry
+      expect(response).to_not be_nil
       expect(response_data).to include(
         "employees" => [
           {
